6f8678796891bb9e00c6084fbb50fcfa2408d223
kaue otto <kaueottoclash@gmail.com>
2023-04-23 13:52:19 -0300

    Feature - Criação de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SigninPageComponent } from './pages/signin-page/signin-page.component';
+import { RecuperarSenhaComponent } from './pages/recuperar-senha/recuperar-senha.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { AreaDoClienteComponent } from './pages/area-do-cliente/area-do-cliente.component';
+
+
+const routes : Routes = [
+  {path: '', component:LoginPageComponent},
+  {path: 'signin', component:SigninPageComponent},
+  {path: 'recuperar-senha', component:RecuperarSenhaComponent},
+  {path: 'contato', component:ContatoComponent},
+  {path: 'area-cliente', component:AreaDoClienteComponent},
+]
 
 @NgModule({
   declarations: [
@@ -12,9 +26,11 @@ import { PagesModule } from './pages/pages.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    RouterModule.forRoot(routes)
 
   ],
+  exports:[RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
