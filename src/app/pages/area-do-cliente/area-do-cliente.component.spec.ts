b02f219de51f64049cd5a407056593b5282f762f
kaue otto <kaueottoclash@gmail.com>
2023-04-23 01:59:32 -0300

    Feature - Criação Area do Cliente

---
+++ src/app/pages/area-do-cliente/area-do-cliente.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AreaDoClienteComponent } from './area-do-cliente.component';
+
+describe('AreaDoClienteComponent', () => {
+  let component: AreaDoClienteComponent;
+  let fixture: ComponentFixture<AreaDoClienteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AreaDoClienteComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AreaDoClienteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/area-do-cliente/area-do-cliente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-area-do-cliente',
+  templateUrl: './area-do-cliente.component.html',
+  styleUrls: ['./area-do-cliente.component.css']
+})
+export class AreaDoClienteComponent {
+
+}
