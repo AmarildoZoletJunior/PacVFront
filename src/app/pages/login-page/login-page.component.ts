aab22b1dd16d06685ad53f4a2d4b5acb4496071b
kaue otto <kaueottoclash@gmail.com>
2023-05-28 15:00:46 -0300

    Feature - Desenvolvimento de TimeOut na pagina de login

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -4,6 +4,8 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { AuthService } from 'src/app/services/Auth/Service/Request/auth.service';
 import { ClientResponse } from 'src/app/services/Client/Interface/client-response';
 import { ClientService } from 'src/app/services/Client/Service/client.service';
+import { interval, Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login-page',
@@ -11,29 +13,70 @@ import { ClientService } from 'src/app/services/Client/Service/client.service';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
-
-  formulario!:FormGroup;
+ 
+  maxAttempts = 5;
+  attemptCount = 0;
+  isBlocked = false;
+  private countdownSubscription: Subscription | undefined;
+  countdownSeconds = 30;
+  showBlockedMessage = false;
+  formulario!: FormGroup;
 
   ngOnInit(): void {
     this.formulario = new FormGroup({
-      email: new FormControl('',[Validators.required,Validators.email]),
-      password: new FormControl('',[Validators.required,Validators.minLength(3)])
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(3)])
     })
   }
 
-   constructor(private authService:AuthService){}
+  constructor(private authService: AuthService) { }
 
+  startCountdown() {
+    this.countdownSubscription = interval(1000)
+      .pipe(take(this.countdownSeconds))
+      .subscribe({
+        next: () => {
+          this.countdownSeconds--;
+        },
+        complete: () => {
+          this.resetCountdown();
+        },
+      });
+  }
 
-    enviarDados(){
-      if(this.formulario.valid){
-        console.log(this.formulario.value)
-        this.authService.AuthClient(this.formulario.value).subscribe(x => {
-          localStorage.setItem("keyToken",x.token);
-          localStorage.setItem("idUser",String(x.clientId));
-        },(error)=>{
-          console.log(error)
-        })
-      }
-      console.log("Ok")
+  resetCountdown() {
+    this.attemptCount = 0;
+    this.isBlocked = false;
+    this.countdownSeconds = 30;
+    this.showBlockedMessage = false;
+    if (this.countdownSubscription) {
+      this.countdownSubscription.unsubscribe();
     }
+  }
+
+  enviarDados() {
+    if (this.isBlocked) {
+      this.showBlockedMessage = true; 
+      return; 
+    }
+    if (this.formulario.valid) {
+      console.log(this.formulario.value);
+      this.authService.AuthClient(this.formulario.value).subscribe(
+        (x) => {
+          localStorage.setItem('keyToken', x.token);
+          localStorage.setItem('idUser', String(x.clientId));
+        },
+        (error) => {
+          console.log(error);
+          this.attemptCount++;
+
+          if (this.attemptCount >= this.maxAttempts) {
+            this.isBlocked = true;
+            this.startCountdown();
+          }
+        }
+      );
+    }
+    console.log('Ok');
+  }
 }
