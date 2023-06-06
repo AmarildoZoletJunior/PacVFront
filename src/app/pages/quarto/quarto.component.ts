40046d881ff2af48c0463290b7d468776b834b44
kaue otto <kaueottoclash@gmail.com>
2023-06-05 23:35:11 -0300

    Feature / Retorno DataPicke DD/MM/YYYY

--- src/app/pages/quarto/quarto.component.ts
+++ src/app/pages/quarto/quarto.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { ActivatedRoute, Route } from '@angular/router';
 import { RoomResponseWithImage } from 'src/app/services/Interfaces/room-with-images';
 import { RoomService } from 'src/app/services/Services/Room/Servico/room.service';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-quarto',
   templateUrl: './quarto.component.html',
   styleUrls: ['./quarto.component.css']
 })
+
 export class QuartoComponent implements OnInit{
   room: RoomResponseWithImage | undefined;
   idRoute!:string | null;
+  
+  startDate!: any;
 
   disabledDates = [
     new Date('2020-02-05'),
@@ -36,7 +40,12 @@ export class QuartoComponent implements OnInit{
     },(error)=>{
       console.log(error)
     })
-
   }
 
+  onDateChange() {
+
+    console.log('startDate: ' + format(new Date(this.startDate[0]), 'dd/MM/yyyy'))
+    console.log('endDate: ' + format(new Date(this.startDate[1]), 'dd/MM/yyyy'))
+
+  }
 }
